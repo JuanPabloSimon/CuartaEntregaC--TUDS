37e8e467c7f124df47c783eb285bb2d3495f1ee4
Juan Pablo <jsimon0301@gmail.com>
2025-05-28 19:50:04 -0300

    Refactorizacion de codigo archivo Colas

--- Colas_Simon-Olarte-Villanueva.cpp
+++ Colas_Simon-Olarte-Villanueva.cpp
@@ -20,15 +20,7 @@ void insertarCola(Nodo *&,Nodo *&,Alumno);
 bool cola_vacia(Nodo *);
 void suprimirCola(Nodo *&, Nodo *&, Alumno &);
 Alumno cargarAlumno();
-
-int seleccion_cola() {
-	int seleccion;
-	cout<<"Seleccione una cola:"<<endl;
-	cout<<"1. Cola N°1"<<endl;
-	cout<<"2. Cola N°2"<<endl;
-	cout<<"Opcion: ";cin>>seleccion;
-	return seleccion;
-}
+int seleccion_cola();
 
 
 int main() {
@@ -74,127 +66,83 @@ void menu() {
 			}
 			case 2: {
 				int cola = seleccion_cola();
-				
-				if (cola == 1) {
-					if (cola_vacia(frente)) {
-					cout << "\nLa cola está vacía." << endl;
-					break;
-					}
-
-					cout << "\nMostrando los elementos de la cola N°1:\n";
-	
-					Nodo *aux = frente;
-					while (aux != NULL) {
-						cout << "Nombre: " << aux->alu.nombre << ", ";
-						cout << "DNI: " << aux->alu.dni << ", ";
-						cout << "Edad: " << aux->alu.edad << endl;
-						aux = aux->siguiente;
-					}
-					break;
-				} else if (cola == 2) {
-					if (cola_vacia(frente2)) {
-					cout << "\nLa cola está vacía." << endl;
-					break;
-					}
-
-					cout << "\nMostrando los elementos de la cola N°2:\n";
-	
-					Nodo *aux = frente2;
-					while (aux != NULL) {
-						cout << "Nombre: " << aux->alu.nombre << ", ";
-						cout << "DNI: " << aux->alu.dni << ", ";
-						cout << "Edad: " << aux->alu.edad << endl;
-						aux = aux->siguiente;
-					}
-					break;
-				} else {
+				Nodo *aux;
+				if (cola == 1) aux = frente;
+				else if (cola == 2) aux = frente2;
+				else {
 					cout<<"Seleccion de cola incorrecta"<<endl;	
+					break;
 				}	
 				
-				
+				if (cola_vacia(aux)) {
+					cout<<"La cola está vacía."<<endl;
+					break;
+				}
+				while (aux != NULL) {
+					cout<<"Nombre: "<<aux->alu.nombre<<", ";
+					cout<<"DNI: "<<aux->alu.dni<< ", ";
+					cout<<"Edad: "<<aux->alu.edad<<endl;
+					aux = aux->siguiente;
+				}
+				break;
 			}
 			case 3: {
 				int cola = seleccion_cola();
+				Nodo *auxFrente;
+				Nodo *auxFin;
 				if (cola == 1) {
-					Nodo *auxFrente = frente;
-					Nodo *auxFin = fin;
-					
-					if (cola_vacia(auxFrente)) {
-						cout<<"\n La cola esta vacia, no hay alumnos para comparar";
-						break;
-					}
-					
-					Alumno alumnoMayor = auxFrente->alu;
-					auxFrente = auxFrente->siguiente;
-					
-					while(auxFrente != NULL) {
-						if (auxFrente-> alu.edad > alumnoMayor.edad) {
-							alumnoMayor = auxFrente->alu;
-						}
-						auxFrente = auxFrente->siguiente;
-					}
-					
-					cout<<"\n El alumno de la cola N°1 con mayor edad es: "<<alumnoMayor.nombre<<", y tiene: "<<alumnoMayor.edad<<" años."<<endl;
-					break;
-				} else if (cola == 2) {
-					Nodo *auxFrente = frente2;
-					Nodo *auxFin = fin2;
-					
-					if (cola_vacia(auxFrente)) {
-						cout<<"\n La cola esta vacia, no hay alumnos para comparar";
-						break;
-					}
-					
-					Alumno alumnoMayor = auxFrente->alu;
-					auxFrente = auxFrente->siguiente;
-					
-					while(auxFrente != NULL) {
-						if (auxFrente-> alu.edad > alumnoMayor.edad) {
-							alumnoMayor = auxFrente->alu;
-						}
-						auxFrente = auxFrente->siguiente;
-					}
-					
-					cout<<"\n El alumno de la cola N°2 con mayor edad es: "<<alumnoMayor.nombre<<", y tiene: "<<alumnoMayor.edad<<" años."<<endl;
-					break;
+					auxFrente = frente;
+					auxFin = fin;
+				} else if(cola ==2) {
+					auxFrente = frente2;
+					auxFin = fin2;
 				} else {
 					cout<<"Seleccion de cola incorrecta"<<endl;	
 				}
+				
+				if (cola_vacia(auxFrente)) {
+						cout<<"\n La cola esta vacia, no hay alumnos para comparar";
+						break;
+				}
+				
+				Alumno alumnoMayor = auxFrente->alu;
+				auxFrente = auxFrente->siguiente;
+				
+				while(auxFrente != NULL) {
+					if (auxFrente-> alu.edad > alumnoMayor.edad) {
+						alumnoMayor = auxFrente->alu;
+					}
+					auxFrente = auxFrente->siguiente;
+				}
+				
+				cout<<"\n El alumno de la cola N"<<cola<<" con mayor edad es: "<<alumnoMayor.nombre<<", y tiene: "<<alumnoMayor.edad<<" años."<<endl;
+				break;
 			}
 				break;
 			case 4: {
 				int cola = seleccion_cola();
-				if (cola == 1) {
-					if (cola_vacia(frente)) {
-						cout<<"\n La cola esta vacia, no hay alumnos.";
-						break;
-					}
-					Nodo *aux = frente;
-					int contador = 0;
-					while(aux != NULL) {
-						contador++;
-						aux = aux->siguiente;
-					}
-					cout<<"La cantidad de alumnos en la cola N1 es de: "<<contador<<endl;
-				} else if (cola == 2) {
-					if (cola_vacia(frente2)) {
-						cout<<"\n La cola esta vacia, no hay alumnos.";
-						break;
-					}
-					Nodo *aux = frente2;
-					int contador = 0;
-					while(aux != NULL) {
-						contador++;
-						aux = aux->siguiente;
-					}
-					cout<<"La cantidad de alumnos en la cola N2 es de: "<<contador<<endl;
-				} else {
+				Nodo *aux;
+				
+				if (cola == 1) aux = frente;
+				else if (cola == 2) aux = frente2;
+				else {
 					cout<<"Seleccion de cola incorrecta"<<endl;	
+					break;
 				}	
+				if (cola_vacia(frente)) {
+					cout<<"\n La cola esta vacia, no hay alumnos.";
+					break;
+				}
+				int contador = 0;
+				while(aux != NULL) {
+					contador++;
+					aux = aux->siguiente;
+				}
+				cout<<"La cantidad de alumnos en la cola N">>cola>>" es de: "<<contador<<endl;
 				break;
 			}
 			case 5: {
-				int cola = seleccion_cola();
+				int cola = seleccion_cola();	
 				if (cola == 1) {
 					cout<<"\Sacando los elementos de la cola N°1:";
 					while(frente != NULL) {
@@ -234,8 +182,6 @@ void menu() {
 	}while(opc != 6);
 };
 
-// Seleccion de cola 
-
 
 // Insertar elementos en la cola
 void insertarCola(Nodo *&frente, Nodo *&fin, Alumno n) {
@@ -252,6 +198,17 @@ void insertarCola(Nodo *&frente, Nodo *&fin, Alumno n) {
 	
 	fin = nuevo_nodo;
 };
+
+
+int seleccion_cola() {
+	int seleccion;
+	cout<<"Seleccione una cola:"<<endl;
+	cout<<"1. Cola N°1"<<endl;
+	cout<<"2. Cola N°2"<<endl;
+	cout<<"Opcion: ";cin>>seleccion;
+	return seleccion;
+}
+
 
 // Determinar si la cola esta vacía
 bool cola_vacia(Nodo *frente) {
